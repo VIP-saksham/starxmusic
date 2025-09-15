2058fc22de1c107dfe3f90ca93c06f4fe6b49545
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-15 23:50:24 +0530

    Update help.py

--- AloneX/utils/inline/help.py
+++ AloneX/utils/inline/help.py
@@ -52,18 +52,6 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb17",),
-                InlineKeyboardButton(text=_["H_B_18"], callback_data="help_callback hb18",),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_19"], callback_data="help_callback hb19",),
-                InlineKeyboardButton(text=_["H_B_20"], callback_data="help_callback hb20",),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_21"], callback_data="help_callback hb21",),
-                InlineKeyboardButton(text=_["H_B_22"], callback_data="help_callback hb22",),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_23"], callback_data="help_callback hb23",),
             ],
             mark,
         ]
