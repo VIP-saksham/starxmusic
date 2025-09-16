3ba55b8c7d587e938fa9eb37c026026a9a17f9c6
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-16 22:09:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,11 +23,11 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
-OWNER_ID = int(getenv("OWNER_ID", "6079943111"))
+OWNER_ID = int(getenv("OWNER_ID", "5635324483"))
 
-OWNER_USERNAME = getenv("OWNER_USERNAME","AloneHuVai")
+OWNER_USERNAME = getenv("OWNER_USERNAME","isamxyz")
 
-BOT_USERNAME = getenv("BOT_USERNAME" , "AloneXMusicBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "StarXMusicBot")
 
 COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
 
@@ -37,15 +37,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/adityabty/Aditya_x_musicbot.git",
+    "https://github.com/VIP-saksham/starxmusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ALONE")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Bollywoodmusicsupport1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Bollywoodmusicsupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THEhellBots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sunshine_gc")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
