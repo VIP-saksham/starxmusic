f27f101efae281d892c3d0033ef9a07550af1ccc
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-16 23:13:31 +0530

    Update mongo.py

--- AloneX/utils/mongo.py
+++ AloneX/utils/mongo.py
@@ -1,1 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from config import MONGO_DB_URI
 
+mongo = MongoCli(MONGO_DB_URI)
+db = mongo["SAM"]
+
+coupledb = db["couple"]
+impdb = db["pretender"]
